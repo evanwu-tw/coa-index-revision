707d2e2a8f1030cc6ff305c1870a0f2f9c59ac0a
Evan <gsscsid@gmail.com>
2014-11-17 01:37:59 +0800

    index main content finsh
    首頁主要內容區塊完成。

--- js/action.js
+++ js/action.js
@@ -1,13 +1,20 @@
-// most hight
-$(function (){
-  $('#most-h').each(function(){
-    var maxHeight = 0;
-    $(this).find(".fn-list-item").each(function(){ 
-      var boxheight=$(this).innerHeight();
-      if( boxheight !== undefined && boxheight > maxHeight) maxHeight = boxheight;
+$(document).ready(function() {
+  // most hight
+  function res_w(){
+    $('#most-h').each(function(){
+      var maxHeight = 0;
+      $(this).find(".fn-list-item").each(function(){ 
+        var boxheight=$(this).innerHeight();
+        if( boxheight !== undefined && boxheight > maxHeight) maxHeight = boxheight;
+      });
+
+      $(this).find(".fn-list-item").css("height", maxHeight - 10);
     });
-    $(this).find(".fn-list-item").css("height", maxHeight - 10);
-  });
+  }
+  res_w();
+  // $(window).resize(function(){
+  //       res_w();
+  // });
 });
 // tab
 $(function () {
